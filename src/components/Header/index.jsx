bc1bbc91d973b01865f711ed2e1add1733cf1d6a
Bernat Canellas Sastre <bernat@wozzo.es>
2024-08-05 16:57:42 +0200

    fix header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MapPopup from '../MapPopUp';
@@ -16,7 +17,13 @@ const Header = ({ changeLocation, selectedDate, onChangeDate }) => {
         <h1 className="header-title">Previsiones Pesca</h1>
         <div className="header-content">
           <button type="button" className="location-button" onClick={openMap}>
-            Seleccionar Ubicación
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2C8.14 2 5 5.14 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.86-3.14-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5 14.5 7.62 14.5 9 13.38 11.5 12 11.5z" />
+            </svg>
+
           </button>
           {isMapOpen && (
           <MapPopup setCoordinates={changeLocation} onClose={closeMap} />
